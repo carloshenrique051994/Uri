a0e048c8a6838bb7bd513e8ec9898add1d54f65a
carloshenrique051994 <carloshenrique051994@users.noreply.github.com>
2020-10-16 20:16:00 -0300

    x

--- pacote-download/Iniciante/JavaScript/uri-1066-ParesImparesPositivosNegativos.js
+++ pacote-download/Iniciante/JavaScript/uri-1066-ParesImparesPositivosNegativos.js
@@ -1,23 +1,20 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
+var input = require('fs').readFileSync('JavaScript/stdin', 'utf8');
 var lines = input.split('\n')
 
-var pares = 0
-var impares = 0
-var positivos = 0
-var negativos = 0
+var pares = impares = positivos = negativos = 0
 
-for(i=0;i<5;i++){
+for (i = 0; i < 5; i++) {
     valor = Number(lines.shift())
-    if(valor%2===0){
+    if (valor % 2 === 0) {
         pares += 1
     }
-    if(valor%2!== 0){
+    if (valor % 2 !== 0) {
         impares += 1
     }
-    if(valor>0){
+    if (valor > 0) {
         positivos += 1
     }
-    if(valor<0){
+    if (valor < 0) {
         negativos += 1
     }
 }

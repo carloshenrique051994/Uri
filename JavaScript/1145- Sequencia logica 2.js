4291a40d185a4b8e13cadabb6b8c7d16d3af078f
carloshenrique051994 <carloshenrique051994@users.noreply.github.com>
2021-01-21 17:51:42 -0300

    att

--- JavaScript/1145- Sequencia logica 2.js
+++
@@ -1,5 +0,0 @@
-var input = require('fs').readFileSync('JavaScript/stdin', 'utf8');
-var lines = input.split('\n')
-
-var valor = lines.shift()
-
---
+++ Python/1146 - SequenciasCrescentes.py
@@ -0,0 +1,5 @@
+while(True){
+    valor = int(input())
+    if valor > 0:
+        
+}
